8507f54bcc7eb0a5cdbe3bef5dfa5f6343690fdb
Max <biryukov.mm@gmail.com>
2020-12-01 16:41:16 +0200

    feature: react material design UI slider

--- src/components/volume.js
+++ src/components/volume.js
@@ -1,16 +1,18 @@
 import React from "react";
+import Slider from "@material-ui/core/slider";
 
 
 function Volume(props) {
-    function volumeHandler(e) {
-        props.setVolume(e.target.value);
-        props.displayHandler("Volume: " + Math.round(e.target.value * 100) + "%");
+    function volumeHandler(e, newValue) {
+        props.setVolume(newValue);
+        // props.displayHandler("Volume: " + Math.round(e.target.value * 100) + "%");
+        props.displayHandler("Volume: " + Math.round(newValue * 100) + "%");
     }
 
     return (
         <div className="volume" id="volume-slider">
             <label htmlFor="volume">Volume</label>
-            <input
+            {/* <input
                 onChange={volumeHandler}
                 id="volume"
                 min="0"
@@ -18,7 +20,13 @@ function Volume(props) {
                 step="0.05"
                 type="range"
                 value={props.volume}
-            ></input>
+            ></input> */}
+            <Slider onChange={volumeHandler}
+                id="volume"
+                min={0}
+                max={1}
+                step={0.05}
+                value={props.volume} aria-labelledby="continuous-slider" />
         </div>)
 }
 
