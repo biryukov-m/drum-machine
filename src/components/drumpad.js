177f4c987584e1b116343a792e323b071c54f1c2
Maximus <biryukov.mm@gmail.com>
2020-12-22 23:50:27 +0200

    feature: working progressbars on each button

--- src/components/drumpad.js
+++ src/components/drumpad.js
@@ -7,6 +7,7 @@ function DrumPad({ clip, clipId, keyCode, keyTrigger, displayHandler, clearDispl
         const drumKeys = ["Q", "W", "E"];
         if (drumKeys.indexOf(keyTrigger) >= 0) {
             drumKeys.forEach((drumKey) => {
+                document.getElementById(drumKey).currentTime = 0;
                 document.getElementById(drumKey).pause();
             });
         }
@@ -15,7 +16,6 @@ function DrumPad({ clip, clipId, keyCode, keyTrigger, displayHandler, clearDispl
         sound.volume = volume;
         sound.currentTime = 0;
         sound.play();
-
         displayHandler(clipId);
         setTimeout(() => clearDisplay(), 500);
     }
@@ -33,13 +33,43 @@ function DrumPad({ clip, clipId, keyCode, keyTrigger, displayHandler, clearDispl
         }
     }
 
+    let percent = 0;
+    let timer;
+    const advanceProgressBar = (duration, element) => {
+
+        let progressBar = document.getElementById(`progress-bar-${keyTrigger}`);
+        let increment = 10 / duration;
+        percent = Math.min(increment * element.currentTime * 10, 100);
+        let percent10 = percent.toFixed(2);
+        progressBar.style.width = (percent > 99) ? 0 : `${percent10}%`;
+        // debug
+        console.log(`Running... ${duration} ${keyTrigger} ${percent}%`)
+        startTimer(duration, element);
+    };
+
+    const startTimer = (duration, element) => {
+        if (percent < 100) {
+            timer = setTimeout(() => { advanceProgressBar(duration, element) }, 100);
+        }
+    }
+
     useEffect(() => {
         const drumPadDiv = document.getElementById(keyTrigger).closest("div");
+        const audio = document.getElementById(keyTrigger);
         document.addEventListener("keydown", handleKeyPress);
         document.addEventListener("keyup", handleKeyUnPress);
         drumPadDiv.addEventListener("mousedown", () => { setPressed(true) });
         drumPadDiv.addEventListener("mouseup", () => { setPressed(false) });
         drumPadDiv.addEventListener("mouseleave", () => { setPressed(false) });
+        audio.addEventListener("playing", (_event) => {
+            let duration = _event.target.duration;
+            advanceProgressBar(duration, audio);
+        });
+        audio.addEventListener("pause", (_event) => {
+            clearTimeout(timer);
+        });
+        audio.addEventListener("ended", () => { clearTimeout(timer) });
+
 
         return (() => {
             document.addEventListener("keydown", handleKeyPress);
@@ -47,6 +77,14 @@ function DrumPad({ clip, clipId, keyCode, keyTrigger, displayHandler, clearDispl
             drumPadDiv.addEventListener("mousedown", () => { setPressed(true) });
             drumPadDiv.addEventListener("mouseup", () => { setPressed(false) });
             drumPadDiv.addEventListener("mouseleave", () => { setPressed(false) });
+            audio.addEventListener("playing", (_event) => {
+                let duration = _event.target.duration;
+                advanceProgressBar(duration, audio);
+            });
+            audio.addEventListener("pause", (_event) => {
+                clearTimeout(timer);
+            });
+            audio.addEventListener("ended", () => { clearTimeout(timer) });
         });
 
     });
@@ -58,14 +96,13 @@ function DrumPad({ clip, clipId, keyCode, keyTrigger, displayHandler, clearDispl
             onClick={playSound}
             id={clipId}
         >
-
             <audio
                 className="clip"
                 id={keyTrigger}
                 src={clip}
             />
             {keyTrigger}
-            <div className="progress-bar" id="progress-bar" />
+            <div className="progress-bar" id={`progress-bar-${keyTrigger}`} />
         </div>
     )
 };
